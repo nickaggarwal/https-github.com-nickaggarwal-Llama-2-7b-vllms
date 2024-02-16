f7d780702e8a76b6eca7076b42c7bf9f8898fd6f
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-16 22:49:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ class InferlessPythonModel:
         self.template = """SYSTEM: You are a helpful assistant.
         USER: {}
         ASSISTANT: """
-        local_path = "/var/nfs-mount/translation-pipeline-volume/model1"
+        local_path = "/var/nfs-mount/translation-pipeline-volume/model2"
         if os.path.exists(local_path) == False :
             os.makedirs(local_path)
             snapshot_download(
                 "meta-llama/Llama-2-7b-chat-hf",
                 local_dir=local_path,
+                local_dir_use_symlinks=False ,
                 token="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",
             )
         self.llm = LLM(
